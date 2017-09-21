5f8dea7a7420b0a35fb2634e35ce5f6b0a98093f
dummyTestUsr <32167909+dummyTestUsr@users.noreply.github.com>
2017-09-21 18:30:55 +0200

    Added User Properties

--- src/main/java/com/testTask/creditCardManager/validations/UserValidator.java
+++ src/main/java/com/testTask/creditCardManager/validations/UserValidator.java
@@ -3,6 +3,7 @@ package com.testTask.creditCardManager.validations;
 import com.testTask.creditCardManager.models.User;
 import com.testTask.creditCardManager.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
@@ -13,6 +14,18 @@ import org.springframework.validation.Validator;
  */
 @Component
 public class UserValidator implements Validator {
+
+    @Value("${user.username.minLength}")
+    private int usernameMinLength;
+
+    @Value("${user.username.maxLength}")
+    private int usernameMaxLength;
+
+    @Value("${user.password.minLength}")
+    private int passwordMinLength;
+
+    @Value("${user.password.maxLength}")
+    private int passwordMaxLength;
 
     @Autowired
     UserService userService;
@@ -29,24 +42,25 @@ public class UserValidator implements Validator {
 
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "username", "Username cannot be empty!");
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "Password cannot be empty!");
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "confirmedPassword", "Password Confirmation cannot be empty!");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "confirmedPassword", "Password Confirmation cannot be " +
+            "empty!");
 
-        if(user.getUsername().length() < 3 || user.getUsername().length() > 10){
-            errors.rejectValue("username", "Length must be between 3 and 10");
+        if (user.getUsername().length() < usernameMinLength || user.getUsername().length() > usernameMaxLength) {
+            errors.rejectValue("username", "Length must be between" + usernameMinLength + " and " + usernameMaxLength);
         }
 
-        if(user.getPassword().length() < 3 || user.getPassword().length() > 20){
-            errors.rejectValue("password", "Length must be between 3 and 10");
+        if (user.getPassword().length() < 3 || user.getPassword().length() > 20) {
+            errors.rejectValue("password", "Length must be between" + passwordMinLength + " and " + passwordMaxLength);
         }
 
         // if password and confirmed password do not match
-        if(!user.getPassword().equals(user.getConfirmedPassword())){
+        if (!user.getPassword().equals(user.getConfirmedPassword())) {
             errors.rejectValue("confirmedPassword", "Passwords do not match!");
         }
 
         // if another user by the same name already exists.
-        if(userService.findByUsername(user.getUsername()) != null){
-            errors.rejectValue("username","Username already exists!");
+        if (userService.findByUsername(user.getUsername()) != null) {
+            errors.rejectValue("username", "Username already exists!");
         }
     }
 }
